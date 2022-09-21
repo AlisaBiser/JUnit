985aa87282bba56460e9d255f9db723f44fc6090
Alesia Abramova <alesja.abramova@gmail.com>
2022-09-21 11:46:30 +0200

    add commit for ivan

---
+++ src/test/java/demoqaalesia/JUnitDemoTestAlesia.java
@@ -0,0 +1,44 @@
+package demoqaalesia;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class JUnitDemoTestAlesia {
+
+    @BeforeAll
+    static void setUp() {
+        System.out.println("### @BeforeAll !");
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.out.println("### @AfterAll !");
+
+
+    }
+
+    @BeforeEach
+    void beforeEachTest() {
+        // open("ya.ru");
+        System.out.println("###     @BeforeEach !");
+    }
+
+    @AfterEach
+    void afterEachTest() {
+        System.out.println("###     @AfterEach !");
+    }
+
+
+        @Test
+    void simpleTest() {
+        System.out.println("###         @Test simpleTest() !");
+        Assertions.assertTrue(3 > 2);
+
+    }
+
+
+}
