19e42a4329f03b12378327f9c8ea0f06a18b6c5b
Alesia Abramova <alesja.abramova@gmail.com>
2022-09-23 10:41:07 +0200

    add commit css xpath examples

---
+++ src/test/java/demoqaalesia/CssXpathExampleAlesia.java
@@ -0,0 +1,11 @@
+package demoqaalesia;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class CssXpathExampleAlesia {
+    void  cssXpathExampleAlesia() {
+        $("#userEmail").setValue("a@a.com");
+        $("#userEmail").setValue("a@a.com");
+    }
+
+}
